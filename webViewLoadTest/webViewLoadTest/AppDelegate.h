9f98b64878f348f46781afcaf368551ec6903e48
Simon_X <Simon_X_CN@outlook.com>
2018-07-21 17:33:47 +0800

    新建

---
+++ webViewLoadTest/webViewLoadTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  webViewLoadTest
+//
+//  Created by Simon_X_CN on 2018/7/21.
+//  Copyright © 2018年 Simon_X_CN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ webViewLoadTest/webViewLoadTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  webViewLoadTest
+//
+//  Created by Simon_X_CN on 2018/7/21.
+//  Copyright © 2018年 Simon_X_CN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
